83835d29757cabfba95ea6cc520d1e9d6a7fc74e
vkmouse <knight31241@gmail.com>
2022-09-18 11:19:03 +0800

    Add better or equal than

--- client/src/Domain/EnchantedAttributeCalculater.test.ts
+++ client/src/Domain/EnchantedAttributeCalculater.test.ts
@@ -58,3 +58,143 @@ test('sum attributes with different isPercentage', () => {
     isPercentage: false
   });
 });
+
+test('equal with different count', () => {
+  const lhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }, {
+    value: 3,
+    name: 'Second Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.equal(lhs, rhs)).toBeTruthy();
+});
+
+test('not equal with different value', () => {
+  const lhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: 4,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.equal(lhs, rhs)).toBeFalsy();
+});
+
+test('not equal with different category', () => {
+  const lhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: false,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.equal(lhs, rhs)).toBeFalsy();
+});
+
+test('better than with positive', () => {
+  const lhs = [{
+    value: 4,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: 3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.betterThan(lhs, rhs)).toBeTruthy();
+});
+
+test('better than with negative', () => {
+  const lhs = [{
+    value: -4,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: -3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.betterThan(lhs, rhs)).toBeTruthy();
+});
+
+test('better than with some equal attibutes', () => {
+  const lhs = [{
+    value: -3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }, {
+    value: 6,
+    name: 'Second Attribute',
+    isPercentage: true,
+  }];
+  const rhs = [{
+    value: -3,
+    name: 'First Attribute',
+    isPercentage: true,
+  }, {
+    value: 5,
+    name: 'Second Attribute',
+    isPercentage: true,
+  }];
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.betterThan(lhs, rhs)).toBeTruthy();
+});
+
+test('better or equal than', () => {
+  const calculater = new EnchantedAttributeCalculater();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -3, name: 'First Attribute', isPercentage: true }],
+    [{ value: -3, name: 'First Attribute', isPercentage: true }]
+  )).toBeTruthy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -4, name: 'First Attribute', isPercentage: true }],
+    [{ value: -3, name: 'First Attribute', isPercentage: true }]
+  )).toBeTruthy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: 4, name: 'First Attribute', isPercentage: true }],
+    [{ value: 3, name: 'First Attribute', isPercentage: true }]
+  )).toBeTruthy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -4, name: 'First Attribute', isPercentage: true }, 
+     { value: 5, name: 'Second Attribute', isPercentage: true }],
+    [{ value: -3, name: 'First Attribute', isPercentage: true },
+     { value: 5, name: 'Second Attribute', isPercentage: true }]
+  )).toBeTruthy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -4, name: 'First Attribute', isPercentage: true }, 
+     { value: 5, name: 'Second Attribute', isPercentage: true }],
+    [{ value: -3, name: 'First Attribute', isPercentage: true }]
+  )).toBeTruthy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -4, name: 'First Attribute', isPercentage: true }, 
+     { value: 5, name: 'Second Attribute', isPercentage: true }],
+    [{ value: -5, name: 'First Attribute', isPercentage: true }]
+  )).toBeFalsy();
+  expect(calculater.betterOrEqualThan(
+    [{ value: -4, name: 'First Attribute', isPercentage: true }, 
+     { value: 5, name: 'Second Attribute', isPercentage: true }],
+    [{ value: -4, name: 'First Attribute', isPercentage: true },
+     { value: -5, name: 'Third Attribute', isPercentage: true }]
+  )).toBeFalsy();
+});
--- client/src/Domain/EnchantedAttributeCalculater.ts
+++ client/src/Domain/EnchantedAttributeCalculater.ts
@@ -15,6 +15,33 @@ class EnchantedAttributeCalculater {
 
     return attributes;
   }
+  
+  equal(lhs: Array<EnchantedAttribute>, rhs: Array<EnchantedAttribute>): boolean {
+    for (const attribute of rhs) {
+      const found = lhs.find(p => this.compare(p, attribute));
+      if (found === undefined || found.value !== attribute.value) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  betterThan(lhs: Array<EnchantedAttribute>, rhs: Array<EnchantedAttribute>): boolean {
+    let isBetter = false;
+    for (const attribute of rhs) {
+      const found = lhs.find(p => this.compare(p, attribute));
+      if (found === undefined || Math.abs(found.value) < Math.abs(attribute.value)) {
+        return false;
+      } else if (Math.abs(found.value) > Math.abs(attribute.value)) {
+        isBetter = true;
+      }
+    }
+    return isBetter;
+  }
+
+  betterOrEqualThan(lhs: Array<EnchantedAttribute>, rhs: Array<EnchantedAttribute>): boolean {
+    return this.equal(lhs, rhs) || this.betterThan(lhs, rhs);
+  }
 
   private compare(lhs: EnchantedAttribute, rhs: EnchantedAttribute): boolean {
     return lhs.name === rhs.name && lhs.isPercentage === rhs.isPercentage;
