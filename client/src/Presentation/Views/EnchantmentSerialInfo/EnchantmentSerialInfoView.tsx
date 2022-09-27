c47c787e4870cd6f6edb5b666812aee93dd10ef7
vkmouse <knight31241@gmail.com>
2022-09-27 13:26:31 +0800

    Update the description of serial info

--- client/src/Presentation/Views/EnchantmentSerialInfo/EnchantmentSerialInfoView.tsx
+++ client/src/Presentation/Views/EnchantmentSerialInfo/EnchantmentSerialInfoView.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import ComponentData from '../../Components/ComponentData';
+import Button from '../../Components/CustomButton';
 import Combobox from '../../Components/CustomCombobox';
 
 interface IProps {
@@ -24,18 +25,42 @@ class EnchantmentSerialInfoView extends React.Component<IProps> {
       <div className='wrapper'>
         <div className='wrapper__title'>附魔系列</div>
         <div className='wrapper__content'>
-          <div>
+          <div className='wrapper__row'>
             <Combobox 
               label='系列名稱'
               data={serialData}
               value={serialId}
               onChange={onSerialChange}
             />
+            <a 
+              style={{ textDecoration: "none", }} 
+              target={"_blank"} 
+              rel="noopener noreferrer" 
+              href={serialUrl}
+            >
+              <Button label='前往官方網站'/>
+            </a>
           </div>
-          <Box sx={{ margin: '0.5em', marginTop: '1.0em' }}>
-            <Box sx={{ fontWeight: 'bold', marginTop: '0.5em' }}>內容說明</Box>
-            <Box sx={{ marginTop: '0.5em' }}>{serialDescription}</Box>
-            <Box sx={{ marginTop: '0.5em' }}><a href={serialUrl}>前往官方網站</a></Box>
+          <Box sx={{ 
+            margin: '0.5em', 
+            marginTop: '1em',
+            '&:hover .child': {
+              textOverflow: "clip", 
+              overflow: "visible" ,
+              whiteSpace: "normal"
+            }
+          }}>
+            <Box sx={{ fontWeight: 'bold' }}>內容說明</Box>
+            <Box className={"child"} sx={{ 
+              cursor: "default",
+              marginTop: '0.5em', 
+              marginBottom: '0.5em', 
+              textOverflow: "ellipsis", 
+              overflow: "hidden" ,
+              whiteSpace: "nowrap"
+            }}>
+              {serialDescription}
+            </Box>
           </Box>
         </div>
       </div>
